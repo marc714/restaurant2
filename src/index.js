9dc429321728a416c8a2cc30088073f3aa2a977f
Marc Vo <vo.marc@gmail.com>
2022-12-23 21:25:46 -0800

    import header logo

--- src/index.js
+++ src/index.js
@@ -8,8 +8,9 @@ const homePage = function(){
 }();
 
 
-//import Logo from './assets/InNOut.png'
+import Logo from './assets/InNOut.png'
 const header = document.querySelector("#header");
 const headerLogo = new Image(100);
-headerLogo.src = '/src/assets/InNOut.png';
+// headerLogo.src = '/src/assets/InNOut.png';  // this doesn't work on Github pages. Must use 'import' and when you do, VS code automatically pulls the images into the 'dist' folder after you npm run build.
+headerLogo.src = Logo;
 header.append(headerLogo);
